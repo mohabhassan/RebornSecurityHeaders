f6fa977f7288b7ba7b8be6c8dc2075cd86ae40b6
mohabhassan <mohabhassan312@gmail.com>
2019-10-04 05:15:20 +0200

    Update RebornAPI.h

--- RebornAPI.h
+++ RebornAPI.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef REBORNSECURITYFIXES_EXPORTS
+#define REBORNSECURITYFIXES_API __declspec(dllexport)
+#else
+#define REBORNSECURITYFIXES_API __declspec(dllimport)
+#endif
+
 class RebornImports
 {
 public:
@@ -11,3 +17,5 @@ class RebornExports
 public:
 
 };
+
+REBORNSECURITYFIXES_API RebornExports* GetRebornAPI(RebornImports* import);
