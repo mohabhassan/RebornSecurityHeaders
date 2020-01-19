67e1f3036aca8480d81fee31ddc24acaf3fb0c03
mohabhassan <mohabhassan312@gmail.com>
2020-01-19 02:40:53 +0200

    Add function definitions p1

--- RebornAPI.h
+++ RebornAPI.h
@@ -2,6 +2,7 @@
 
 #ifdef REBORNSECURITYFIXES_EXPORTS
 #define REBORNSECURITYFIXES_API __declspec(dllexport)
+#include "dgamex86.h"
 #else
 #define REBORNSECURITYFIXES_API __declspec(dllimport)
 #endif
@@ -10,12 +11,40 @@ class RebornImports
 {
 public:
 
+	void*	(*MemoryMalloc)(int size);
+	void	(*MemoryFree)(void* ptr);
+
+	cvar_t* (*Cvar_Get)(const char* varName, const char* varValue, int varFlags);
+
+	int (*Milliseconds)();
+
+	void (*KickClient)(int client_num, const char* reason, const char* public_reason);
+
+	void (*Printf)(const char* format, ...);
+	void (*centerprintf)(gentity_t* ent, const char* format, ...);
+
+
+	bool HookFunction(_Inout_ PVOID* ppPointerOriginal, _In_ PVOID pDetour);
 };
 
 class RebornExports
 {
 public:
+	//Reborn should return reason string to deny player or NULL to allow player.
+	char*	(*RB_PreClientConnect)(int clientNum, qboolean firstTime);
+
+	//Reborn should return false to stop executing ClientThink or true to allow execution.
+	bool	(*RB_PreClientThink)(gentity_t* ent, userCmd_t* ucmd, userEyes_t* eyeInfo);
+
+	//Nothing special here.
+	void	(*RB_ClientUserinfoChanged)(gentity_t* ent, char* userInfo);
+
+	//Nothing special here.
+	void	(*RB_InitGame)(int startTime, int randomSeed);
+
+	//Nothing special here.
+	void	(*RB_ClientDisconnect)(gentity_t* ent);
 
 };
 
-REBORNSECURITYFIXES_API RebornExports* GetRebornAPI(RebornImports* import);
+REBORNSECURITYFIXES_API RebornExports* GetRebornAPI(RebornImports* imports);
